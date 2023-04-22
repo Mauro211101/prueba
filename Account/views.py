c92702e91591d273c2e24d9334a74b1b10fd7be1
Mauro <mauval321@hotmail.com>
2023-04-22 12:02:26 -0300

    Autenticacion de usuarios

--- Account/views.py
+++ Account/views.py
@@ -3,14 +3,13 @@ from django.views.generic import ListView,CreateView,UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.mixins import LoginRequiredMixin  
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 class SignUp(CreateView):
     form_class = UserCreationForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("index")
-
 
 class Login(LoginView):
     next_page = reverse_lazy("post-list")
--- miBlog/views.py
+++ miBlog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from miBlog.models import Post
 from django.views.generic import ListView,CreateView,DetailView,DeleteView,UpdateView
 from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def index(request):
@@ -19,13 +19,31 @@ class AddPost(LoginRequiredMixin, CreateView):
     success_url =  reverse_lazy("post-list")
     fields = '__all__'
 
-class UpdatePost(LoginRequiredMixin, UpdateView):
+class UpdatePost(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
     model = Post
     success_url = reverse_lazy("post-list")
     fields = '__all__'
 
+    def test_func(self):
+        user_id = self.request.user.id
+        post_id = self.kwargs.get("pk")
+        return Post.objects.filter(publisher=user_id, id=post_id).exists()
+    
+    def handle_no_permission(self):
+        return render(self.request,"Account/not_found.html")
+
+
+
 class DeletePost(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy("post-list")
+
+    def test_func(self):
+        user_id = self.request.user.id
+        post_id = self.kwargs.get("pk")
+        return Post.objects.filter(publisher=user_id, id=post_id).exists()
+    
+    def handle_no_permission(self):
+        return render(self.request,"Account/not_found.html")
     
 
